78fd0642906a0a8994c15dcd9c727b5f89cafae0
hasindu sithmin <hasindusithmin64@gmail.com>
2022-09-18 15:11:00 +0000

    Update index.html

--- static/index.js
+++ static/index.js
@@ -1,2 +1,23 @@
 
-console.log('Hello World');
+fetch('/quiz')
+    .then(res=>res.json())
+    .then(data=>{
+        for (const {question,answer,options,id} of data) {
+            const li = document.createElement('li')
+            li.className = 'w3-card-4'
+            const div = document.createElement('div')
+            div.innerHTML = question
+            const opts = options.split(',')
+            const select = document.createElement('select')
+            select.className = 'w3-input'
+            for (const opt of opts) {
+                const option = document.createElement('option')
+                option.innerText = opt
+                option.value = opt
+                select.appendChild(option)
+            }
+            li.appendChild(div)
+            li.appendChild(select)
+            document.getElementById('root').appendChild(li)
+        }
+    })
